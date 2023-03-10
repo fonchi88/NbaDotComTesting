84b469f83719a8cc6d404ce38d162eba7ca220a8
francisco.morelos <francisco.moreloso@gmail.com>
2023-03-09 21:43:15 -0700

    steps screenshot functionality

--- src/test/java/step_definitions/Hooks.java
+++ src/test/java/step_definitions/Hooks.java
@@ -5,8 +5,15 @@ import driver.Browser;
 import driver.DriverManager;
 import driver.IDriver;
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import utils.Log;
+
+import java.io.File;
 
 public class Hooks {
 
@@ -28,5 +35,17 @@ public class Hooks {
     @After
     public void AfterSteps(Scenario scenario){
         dm.dispose();
+    }
+
+    @AfterStep
+    public void afterSteps(Scenario scenario){
+        try{
+            File screenshot = ((TakesScreenshot)DriverManager.get(Browser.REMOTE)).getScreenshotAs(OutputType.FILE);
+            byte[] fileContent = FileUtils.readFileToByteArray(screenshot);
+            scenario.attach(fileContent,"image/png","screenshot");
+        }catch(Exception e){
+            Log.error("Unable to attach screenshot: "+e);
+        }
+
     }
 }
